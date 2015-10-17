5301ad7f811f006c3a4b6427d266c6494a4063b9
Martin Blom <martin@blom.org>
2015-10-17 13:08:12 +0200

    Added launchd plist.

--- Formula/esxx.rb
+++ Formula/esxx.rb
@@ -2,9 +2,10 @@ class Esxx < Formula
   desc "An easy-to-use, yet powerful, JavaScript Application Server written in Java."
   homepage "http://esxx.org/"
   url "https://github.com/LeviticusMB/esxx/archive/0.9.20151017.tar.gz"
-  head "https://github.com/LeviticusMB/esxx.git"
   version "0.9.20151017"
   sha256 "64d145481a6c2f3a9ec909950623321fea051769590ecaf59a1f300671015c8d"
+  head "https://github.com/LeviticusMB/esxx.git"
+  revision 1
 
   depends_on "ant" => :build
   depends_on :java => "1.6"
@@ -13,6 +14,31 @@ class Esxx < Formula
     system "ant", "-verbose", "-Dskip-install-mac=1",
            "-Dprefix=#{prefix}", "-Dsysconfdir=#{etc}", "-Dlocalstatedir=#{var}", "-Dsharedstatedir=#{var}",
            "generate-build-files", "install"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+
+        <key>ProgramArguments</key>
+        <array>
+         <string>#{bin}/esxx</string>
+           <string>--http</string>
+           <string>7777</string>
+        </array>
+
+        <key>KeepAlive</key>
+        <true/>
+
+        <key>WorkingDirectory</key>
+        <string>/usr/local/share/doc/esxx/examples/</string>
+      </dict>
+    </plist>
+    EOS
   end
 
   test do
